b07d551bd82f6b828b886b3dc589d289d9190289
Ard van Breemen <ard@kwaak.net>
2024-03-11 07:48:17 +0100

    try graphiz

--- graphviz/essphase1.lua
+++ graphviz/essphase1.lua
@@ -29,35 +29,57 @@ local function mpii(tag,location,title)
 ]]
 	return (gs(data,{TAG=tag,LOCATION=location,TITLE=title}))
 end
+local function busbar(tag,location,size,title)
+	local pre=[[
+	@_TAG_@ [ pos="@_LOCATION_@" shape=plain label=<
+<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+<TR> <TD>@_TITLE_@</TD>
+]]
+	local post=[[
+	</TR>
+	</TABLE> >];
+	]]
+	local s={pre}
+	for i = 1,size do
+		s[#s+1]=([[<TD PORT="%d">%d</TD>]]):format(i,i)
+	end
+	s[#s+1]=post
+	return gs(table.concat(s),{TAG=tag,LOCATION=location,TITLE=title})
+end
 print[[
 digraph Power {
+	splines = true;
 	graph [
 		rankdir ="TB"
 		ranksep ="2"
 	];
+	{
+		node [ shape="rectangle" ];
 ]]
 for p = 0,2 do
 	for m=0,1 do
 		local M={"M","S"}
-		local tag=("VMPL%d%s"):format(p+1,M[m+1])
+		local tag=("VML%d%s"):format(p+1,M[m+1])
 		local title=("MPII L%d %s"):format(p+1,M[m+1])
-		location=("%d,%d!"):format(10,(p*2+m)*3)
+		location=("%d,%d!"):format((p*2+m)*3,10)
 		print(mpii(tag,location,title))
 	end
 end
+print(busbar("INL1",("0,2!"),4,"INL1"))
+print(busbar("INL2",("1,3!"),4,"INL2"))
+print(busbar("INL3",("2,4!"),4,"INL3"))
+print(busbar("INN",("3,5!"),8,"INN"))
+print(busbar("INE",("4,6!"),8,"INE"))
+print(busbar("OUT1L1",("5,2!"),4,"OUT1L1"))
+print(busbar("OUT1L2",("6,3!"),4,"OUT1L2"))
+print(busbar("OUT1L3",("7,4!"),4,"OUT1L3"))
+print(busbar("OUT1N",("8,5!"),8,"OUT1N"))
+print(busbar("OUT1E",("9,6!"),8,"OUT1E"))
 print[[
 	Elem1
 	Elem2
 	Elem3
 	Elem4
-	BusbarIn1L1 [ pos="0,2!" ];
-	BusbarIn1L2 [ pos="1,3!" ];
-	BusbarIn1L3 [ pos="2,4!" ];
-	BusbarIn1N [ pos="3,5!" ];
-	BusbarOut1L1
-	BusbarOut1L2
-	BusbarOut1L3
-	BusbarOut1N
 	CEE3PNI
 	CEE3PNP
 	LynxDist1
@@ -67,56 +89,56 @@ print[[
 	}
 	{
 		edge [ color = "brown" ]
-	CEE3PNI->BusbarIn1L1
-	VML1M:ACINP->BusbarIn1L1
-	VML1S:ACINP->BusbarIn1L1
-	VML1M:ACO1P->BusbarOut1L1
-	VML1S:ACO1P->BusbarOut1L1
-	BusbarOut1L1->CEE3PNP
+	CEE3PNI->INL1:1
+	VML1M:ACINP->INL1:2
+	VML1S:ACINP->INL1:3
+	VML1M:ACO1P->OUT1L1:2
+	VML1S:ACO1P->OUT1L1:3
+	OUT1L1->CEE3PNP
 	}
 	{
 		edge [ color = "black" ]
-	CEE3PNI->BusbarIn1L2
-	VML2M:ACINP->BusbarIn1L2
-	VML2S:ACINP->BusbarIn1L2
-	VML2M:ACO1P->BusbarOut1L2
-	VML2S:ACO1P->BusbarOut1L2
-	BusbarOut1L2->CEE3PNP
+	CEE3PNI->INL2
+	VML2M:ACINP->INL2:2
+	VML2S:ACINP->INL2:3
+	VML2M:ACO1P->OUT1L2:2
+	VML2S:ACO1P->OUT1L2:3
+	OUT1L2->CEE3PNP
 	}
 	{
 		edge [ color = "darkgray" ]
-	CEE3PNI->BusbarIn1L3
-	VML3M:ACINP->BusbarIn1L3
-	VML3S:ACINP->BusbarIn1L3
-	VML3M:ACO1P->BusbarOut3L2
-	VML3S:ACO1P->BusbarOut3L2
-	BusbarOut1L3->CEE3PNP
+	CEE3PNI->INL3
+	VML3M:ACINP->INL3:2
+	VML3S:ACINP->INL3:3
+	VML3M:ACO1P->OUT1L3:2
+	VML3S:ACO1P->OUT1L3:3
+	OUT1L3->CEE3PNP
 	}
 	{
 		edge [ color = "blue" ]
-		CEE3PNI->BusbarIn1N
-		VML1M:ACINN->BusbarIn1N
-		VML1S:ACINN->BusbarIn1N
-		VML2M:ACINN->BusbarIn1N
-		VML2S:ACINN->BusbarIn1N
-		VML3M:ACINN->BusbarIn1N
-		VML3S:ACINN->BusbarIn1N
-		VML1M:ACO1N->BusbarOut1N
-		VML1S:ACO1N->BusbarOut1N
-		VML2M:ACO1N->BusbarOut1N
-		VML2S:ACO1N->BusbarOut1N
-		VML3M:ACO1N->BusbarOut1N
-		VML3S:ACO1N->BusbarOut1N
-		BusbarOut1N->CEE3PNP
+		CEE3PNI->INN:1
+		VML1M:ACINN->INN:2
+		VML1S:ACINN->INN:3
+		VML2M:ACINN->INN:4
+		VML2S:ACINN->INN:5
+		VML3M:ACINN->INN:6
+		VML3S:ACINN->INN:7
+		VML1M:ACO1N->OUT1N:2
+		VML1S:ACO1N->OUT1N:3
+		VML2M:ACO1N->OUT1N:4
+		VML2S:ACO1N->OUT1N:5
+		VML3M:ACO1N->OUT1N:6
+		VML3S:ACO1N->OUT1N:7
+		OUT1N:1->CEE3PNP
 	}
 	{
-		edge [ stype="dashed" color = "yellow" fillcolor="green" ]
-		VML1M->EarthBar
-		VML1S->EarthBar
-		VML2M->EarthBar
-		VML2S->EarthBar
-		VML3M->EarthBar
-		VML3S->EarthBar
+		edge [ style="dashed" color = "yellow" fillcolor="green" ]
+		VML1M:ACINE->INE:2
+		VML1S:ACINE->INE:3
+		VML2M:ACINE->INE:4
+		VML2S:ACINE->INE:5
+		VML3M:ACINE->INE:6
+		VML3S:ACINE->INE:7
 	}
 }
 ]]
